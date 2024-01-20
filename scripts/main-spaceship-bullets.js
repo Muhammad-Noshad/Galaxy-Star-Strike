661fda58054e1f35d59504311f10a878873d7fb5
Muhammad Noshad <muhammadnoshad137@gmail.com>
2024-01-20 18:32:24 +0500

    main spaceship bullets done

--- scripts/main-spaceship-bullets.js
+++ scripts/main-spaceship-bullets.js
@@ -1,37 +1,38 @@
 import { pxToNum } from "./utils/convert-px-to-number.js";
 import { getMainSpaceshipX, getMainSpaceshipY } from "./utils/get-main-spaceship-coordinates.js";
 
-let bulletCounter = 0;
-let bulletElements = [];
+class Bullet {
+  constructor() {
+    this.bulletElement = document.createElement('img');
+    this.bulletElement.src = "Main_Spaceship/Main ship weapons/bullets/bullet-1.png";
+    this.bulletElement.alt = "bullets.png";
+    this.bulletElement.className = "main-spaceship-bullets-img";
+    document.querySelector('.main-spaceship-bullets').appendChild(this.bulletElement);
 
-export function shootBullet(){
-  let bulletHTML = 
-  `
-  <img src="Main_Spaceship/Main ship weapons/bullets/bullet-1.png" alt="bullets.png" class="main-spaceship-bullets-img js-bullet-${++bulletCounter}">
-  `;
+    this.x = pxToNum(getMainSpaceshipX()) + 12;
+    this.y = pxToNum(getMainSpaceshipY()) - 12;
 
-  document.querySelector('.main-spaceship-bullets').innerHTML += bulletHTML;
+    this.bulletElement.style.setProperty('margin-left', `${this.x}px`);
+    this.bulletElement.style.setProperty('margin-top', `${this.y}px`);
 
-  const bulletElement = document.querySelector(`.js-bullet-${bulletCounter}`);
-
-  bulletElements.push(bulletElement);
-
-  let x = getMainSpaceshipX();
-  let y = getMainSpaceshipY();
-
-  x = pxToNum(x);
-  y = pxToNum(y);
-
-  // Adjusting the bullet spawn position
-  x += 12;
-  y -= 12;
-
-  bulletElement.style.setProperty('margin-left', `${x}px`);
-  bulletElement.style.setProperty('margin-top', `${y}px`);
-
-  setInterval(() => { moveBullet() }, 50);
-
-  function moveBullet(){
-    bulletElement.style.setProperty('margin-top', `${y -= 10}px`);
+    this.bulletInterval = setInterval(() => this.moveBullet(), 50);
   }
+
+  moveBullet() {
+    this.y -= 10;
+    this.bulletElement.style.setProperty('margin-top', `${this.y}px`);
+
+    if (this.y <= -30) {
+      this.destroy();
+    }
+  }
+
+  destroy() {
+    clearInterval(this.bulletInterval);
+    this.bulletElement.remove();
+  }
+}
+
+export function shootBullet() {
+  new Bullet();
 }
